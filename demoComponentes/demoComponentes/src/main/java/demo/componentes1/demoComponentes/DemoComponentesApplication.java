a818e786f3da210794882a6fc55a7356a79c0972
jazmin0226 <jariass@ucenfotec.ac.cr>
2020-10-31 21:09:52 -0600

    Initial commit

---
+++ demoComponentes/demoComponentes/src/main/java/demo/componentes1/demoComponentes/DemoComponentesApplication.java
@@ -0,0 +1,13 @@
+package demo.componentes1.demoComponentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoComponentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoComponentesApplication.class, args);
+	}
+
+}
---
+++ demoComponentes/demoComponentes/src/test/java/demo/componentes1/demoComponentes/DemoComponentesApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.componentes1.demoComponentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoComponentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
